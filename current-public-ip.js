7df95536396d016e037a1786481bd973b7756e0b
Quaitzel <github@wt8.eu>
2022-11-14 15:31:57 +0100

    rename and cleanup

--- current-public-ip.js
+++ current-public-ip.js
@@ -1,16 +1,16 @@
-let api = await randomAPI() 
-let widget = await createWidget(api) 
- 
+let responseJson = await jsonKeys() 
+let widget = await createWidget(responseJson) 
+
 if (config.runsInWidget) { 
     Script.setWidget(widget) 
 }  
 else { 
     widget.presentMedium() 
 } 
- 
+
 Script.complete() 
-async function createWidget(api) { 
-    let title = "Titel" 
+
+async function createWidget(responseJson) { 
     let widget = new ListWidget() 
     let gradient = new LinearGradient() 
     gradient.locations = [0, 1] 
@@ -18,26 +18,25 @@ async function createWidget(api) {
         new Color("141414"), 
         new Color("13233F") 
     ] 
- 
     widget.backgroundGradient = gradient 
  
     let titleStack = widget.addStack() 
- 
     titleStack.addSpacer(4) 
-    let titleElement = titleStack.addText(api.url) 
-    titleElement.textColor = Color.white() 
-    titleElement.textOpacity = 0.7 
-    titleElement.font = Font.mediumSystemFont(13) 
+    let organisationElement = titleStack.addText(responseJson.organisation) 
+    organisationElement.textColor = Color.white() 
+    organisationElement.textOpacity = 0.7 
+    organisationElement.font = Font.mediumSystemFont(13) 
     widget.addSpacer(12) 
  
-    let nameElement = widget.addText(api.name) 
-    nameElement.textColor = Color.white() 
-    nameElement.font = Font.boldSystemFont(18) 
+    let ipElement = widget.addText(responseJson.ip) 
+    ipElement.textColor = Color.white() 
+    ipElement.font = Font.boldSystemFont(18) 
     widget.addSpacer(2) 
-    let descriptionElement = widget.addText(api.description) 
-    descriptionElement.minimumScaleFactor = 0.5 
-    descriptionElement.textColor = Color.white() 
-    descriptionElement.font = Font.systemFont(18) 
+
+    let countryElement = widget.addText(responseJson.country) 
+    countryElement.minimumScaleFactor = 0.5 
+    countryElement.textColor = Color.white() 
+    countryElement.font = Font.systemFont(18) 
  
     if (!config.runsWithSiri) { 
         widget.addSpacer(8) 
@@ -45,25 +44,24 @@ async function createWidget(api) {
  
     return widget 
 } 
-async function randomAPI() { 
+async function jsonKeys() { 
     let json = await getJson() 
-    let jsonKey = Object.keys(json) 
  
     return { 
-        name: json["query"], 
-        description: json["country"], 
-        url: json["org"] 
+        ip: json["query"], 
+        country: json["country"], 
+        organisation: json["org"] 
     } 
 } 
  
 async function getJson() { 
-    let url = "http://ip-api.com/json" 
-    let req = new Request(url) 
+    let organisation = "http://ip-api.com/json" 
+    let req = new Request(organisation) 
     return await req.loadJSON() 
 } 
  
 async function loadAppIcon() { 
-    let url = "https://is5-ssl.mzstatic.com/image/thumb/Purple124/v4/21/1e/13/211e13de-2e74-4221-f7db-d6d2c53b4323/AppIcon-1x_U007emarketing-0-7-0-85-220.png/540x540sr.jpg" 
-    let req = new Request(url) 
+    let organisation = "https://is5-ssl.mzstatic.com/image/thumb/Purple124/v4/21/1e/13/211e13de-2e74-4221-f7db-d6d2c53b4323/AppIcon-1x_U007emarketing-0-7-0-85-220.png/540x540sr.jpg" 
+    let req = new Request(organisation) 
     return req.loadImage() 
 }
